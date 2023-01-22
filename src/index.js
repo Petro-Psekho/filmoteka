c3d50c845f57445c6ab24995f96d45c505c39278
Petro Psekho <psekho.pp@gmail.com>
2023-01-22 13:53:22 +0200

    changes the markup

--- src/index.js
+++ src/index.js
@@ -20,8 +20,10 @@ const ID_URL = `${BASE_URL}/movie/`;
 async function movieTrending() {
   try {
     const response = await axios.get(`${TREND_URL}?api_key=${API_KEY}`);
+    console.log(response);
     const arr = response.data;
 
+    console.log(arr);
     console.log(arr.results);
     console.log(arr.results[0]);
     console.log(arr.results[0].poster_path);
@@ -39,11 +41,13 @@ function rendersMarkup(arr) {
   console.log('arr', arr.results);
   const markup = arr.results
     .map(result => {
-      return `<div>
-                <img src="https://www.themoviedb.org/t/p/w300${result.poster_path}" alt="">
-                <h2>${result.title}</h2>
-                <p></p>
-              </div>`;
+      return `<li>
+                <article>
+                  <img src="https://www.themoviedb.org/t/p/w500${result.poster_path}" loading="lazy" alt="${result.title}">
+                  <h2  data-id="${result.id}">${result.title}</h2>
+                  <p></p>
+              </article>
+            </li>`;
     })
     .join('');
 
