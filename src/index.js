1dc5dbf710c888be39aec06e8c82998d4d15e214
Petro Psekho <psekho.pp@gmail.com>
2023-01-23 17:39:53 +0200

    adds function movieGenres

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 const inputQuery = document.querySelector('.js-search');
 inputQuery.addEventListener('submit', onSearch);
@@ -12,22 +13,29 @@ function onSearch(e) {
 }
 
 const API_KEY = 'a148ed5961285512fd3954af576af4a0';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const TREND_URL = `${BASE_URL}/trending/movie/week`;
-const SEARCH_URL = `${BASE_URL}/search/movie`;
-const ID_URL = `${BASE_URL}/movie/`;
+
+const TREND_URL = `/trending/movie/week`;
+const GENRES_URL = `/genre/movie/list`;
 
 async function movieTrending() {
   try {
     const response = await axios.get(`${TREND_URL}?api_key=${API_KEY}`);
-    console.log(response);
-    const arr = response.data;
 
-    console.log(arr);
-    console.log(arr.results);
-    console.log(arr.results[0]);
-    console.log(arr.results[0].poster_path);
-    console.log(arr.results[0].title);
+    const arr = response.data;
+    // console.log(arr);
+    // console.log(arr.results);
+    // console.log(arr.results[0]);
+    // console.log(arr.results[0].poster_path);
+    // console.log(arr.results[0].title);
+    // console.log(arr.results[0].genre_ids[1]);
+
+    // const arrGenres = genres.data;
+    // console.log('genres', arrGenres.genres);
+    // console.log('genresID', arrGenres.genres[1].id);
+
+    // if (arr.results[0].genre_ids[1] === arrGenres.genres[1].id) {
+    //   console.log('genresName', arrGenres.genres[1].name);
+    // }
 
     return arr;
   } catch (error) {
@@ -35,23 +43,52 @@ async function movieTrending() {
   }
 }
 
+async function movieGenres() {
+  try {
+    const genres = await axios.get(
+      `${GENRES_URL}?api_key=${API_KEY}&language=en-US`
+    );
+
+    const arrGenres = genres.data;
+    console.log('genres', arrGenres.genres);
+    // console.log('genresID', arrGenres.genres[1].id);
+
+    // if (arr.results[0].genre_ids[1] === arrGenres.genres[1].id) {
+    //   console.log('genresName', arrGenres.genres[1].name);
+    // }
+
+    return arrGenres;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+Promise.allSettled([movieGenres(), movieTrending()]).then(results =>
+  results.forEach(result => console.log('result.value.genres', result.value))
+);
+
 movieTrending().then(rendersMarkup).catch(console.error());
 
 function rendersMarkup(arr) {
   console.log('arr', arr.results);
+
   const markup = arr.results
     .map(result => {
       return `<li>
                 <article>
-                  <img src="https://www.themoviedb.org/t/p/w500${result.poster_path}" loading="lazy" alt="${result.title}">
+                  <img src="https://www.themoviedb.org/t/p/w200${result.poster_path}" loading="lazy" alt="${result.title}">
                   <h2  data-id="${result.id}">${result.title}</h2>
-                  <p></p>
+                    <div class="js-genres">
+                       <p>${result.genre_ids}</p>
+                       <p>${result.release_date}</p>
+                    </div>
               </article>
             </li>`;
     })
     .join('');
 
   trendingGallery.insertAdjacentHTML('beforeend', markup);
+  console.log(arr.results);
 }
 
 // rendersMarkup();
